0cfbc2bd107d17cca5f9d25dc910ec4e654abd53
Mila Frerichs <mila.frerichs@gmail.com>
2013-05-13 11:56:46 +0200

    changed order

--- app/controllers/vorlage_controller.rb
+++ app/controllers/vorlage_controller.rb
@@ -1,6 +1,6 @@
 class VorlageController < ApplicationController
   def index
     @base_url = "https://www.stadt-muenster.de/sessionnet/sessionnetbi/"
-    @vorlagen = Vorlage.all
+    @vorlagen = Vorlage.order("updated_at DESC").all
   end
 end
