91f9db025a82161e530a643816d88b74b69ff8fd
Mila Frerichs <mila.frerichs@gmail.com>
2013-05-18 14:19:56 +0200

    added basic Vorlgane views and search for Vorlagen Typ

--- app/controllers/vorlage_controller.rb
+++ app/controllers/vorlage_controller.rb
@@ -1,6 +1,9 @@
 class VorlageController < ApplicationController
   def index
-    @base_url = "https://www.stadt-muenster.de/sessionnet/sessionnetbi/"
     @vorlagen = Vorlage.order("updated_at DESC").all
+  end
+
+  def show
+    @vorlage = Vorlage.find(params[:id])
   end
 end
--- app/models/vorlage.rb
+++ app/models/vorlage.rb
@@ -7,4 +7,13 @@ class Vorlage < ActiveRecord::Base
   has_many :sitzung, through: :sitzung_vorlage
   has_many :sitzung_vorlage
 
+  scope :kommende, lambda { where("created_at <= ?", Time.zone.now.beginning_of_day ).order("created_at ASC").limit(5) }
+
+  def base_url
+    "https://www.stadt-muenster.de/sessionnet/sessionnetbi/"
+  end
+
+  def typ_for_gremium(gremium)
+    self.sitzung_vorlage.joins(:sitzung).where("sitzung.gremium_id = ?",gremium.id).first.typ
+  end
 end
