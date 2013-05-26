00ee77fa947bb2a49887ae744a9c027d05e02643
Mila Frerichs <mila.frerichs@gmail.com>
2013-05-26 14:12:43 +0200

    added subjects to index page and changed vorlagen call

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   def index
-    @vorlagen = Vorlage.kommende
+    @vorlagen = Vorlage.letzte.limit(5)
     @sitzungen_heute = Sitzung.heute
     @sitzungen_morgen = Sitzung.morgen
     @sitzungen_woche = Sitzung.kommende_nach_morgen
+    @last_themen = Subject.last_month
+    @next_themen = Subject.next_month
   end
 
   def impressum
