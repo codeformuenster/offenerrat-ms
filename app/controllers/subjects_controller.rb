6f0adf3c91958cd25442e49f8905b510d278592d
Mila Frerichs <mila.frerichs@gmail.com>
2013-06-03 20:12:18 +0200

    added Atom and RSS feeds for subjects
    added search bar in navi

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,4 +2,12 @@ class SubjectsController < ApplicationController
   def index
     @subjects = Subject.all
   end
+
+  def show
+    respond_to do |format|
+       format.rss { render :layout => false }
+       format.html
+       format.atom { @subject = Subject.find(params[:id]) }
+    end
+  end
 end
