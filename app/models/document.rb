6877c852d24220c8b836bd294c8d6e2b4012e56c
Mila Frerichs <mila.frerichs@gmail.com>
2013-06-10 08:38:10 +0200

    search in documents
    added pg_search for documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,15 @@
 class Document < ActiveRecord::Base
   belongs_to :vorlage
+
+  include PgSearch
+  multisearchable :against => [:title, :html]
+
+  def vorlagen_title
+    self.vorlage.title
+  end
+
+  def self.resource_index_attributes
+    [:title, :vorlagen_title]
+  end
+
 end
