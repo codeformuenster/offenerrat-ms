a9bf0632fda03b267bfaa860a2611ca2a298f8ab
Mila Frerichs <mila.frerichs@gmail.com>
2014-10-30 12:36:36 +0100

    Delete empty tests; add spec_helper

--- spec/controllers/vorlage_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe VorlageController do
-
-end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+
+  config.order = "random"
+end
+
