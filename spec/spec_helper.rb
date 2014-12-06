71a1851bbf4dbe0a7efd16a6cde3b1fb42fe7804
Mila Frerichs <mila.frerichs@gmail.com>
2014-12-06 14:54:23 +0100

    configure vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'vcr'
 require 'rspec/autorun'
-
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
